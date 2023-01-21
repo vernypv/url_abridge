ce6ce165a9a32f51e34b76f17c34ef1a6a5a4e26
Verny <pablopimentelvillar@outlook.com~>
2023-01-21 10:08:13 -0500

    My FirstCommit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+print(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return "This is a url_abridge"
