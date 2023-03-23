60f5d4d3ef03a394c47c2a9bf6bf73a37141c24b
Verny <pablopimentelvillar@outlook.com~>
2023-03-23 17:08:36 -0400

    Update JSON conflict parsing data

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json
+import os.path
+#from werkzeug import secure_filename
+
 
 app = Flask(__name__)
 
@@ -13,6 +16,17 @@ def home():
 def your_url():
     if request.method == "POST":
         urls = {}
+        
+          
+        if os.path.exists("urls.json"):
+            with open("urls.json") as urls_file:
+                urls = json.load(urls_file)
+
+        if request.form["code"] in urls.keys():
+            #flash("That short name has already been taken. Please select another name.")
+            return redirect(url_for("home"))
+
+
         urls[request.form["code"]] = {"url":request.form["url"]}
         with open("urls.json","w") as url_file:
             json.dump(urls, url_file)
