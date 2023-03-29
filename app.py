688e7c8cae510920909c18c08c741f8bdbf3c130
Verny <pablopimentelvillar@outlook.com~>
2023-03-29 17:25:33 -0400

    Start template and working w/ sessions cookies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, abort
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -8,7 +8,7 @@ app.secret_key = "h432hi5ohi3h5i5hi3o2hi"
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", codes=session.keys())
 
 @app.route("/your-url", methods=["GET","POST"])
 def your_url():
@@ -34,6 +34,7 @@ def your_url():
 
         with open("urls.json","w") as url_file:
             json.dump(urls, url_file)
+            session[request.form["code"]] = True
         return render_template("your_url.html", code=request.form["code"])
     else:
         return redirect(url_for("home"))
